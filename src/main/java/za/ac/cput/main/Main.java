054197e1fafb6d804c4ceb4b6c8ff9c6500f8565
Jody Kearns <kearnsjody@gmail.com>
2022-03-26 16:58:34 +0200

    First commit, empty project

---
+++ src/main/java/za/ac/cput/main/Main.java
@@ -0,0 +1,4 @@
+package za.ac.cput.main;
+
+public class Main {
+}
---
+++ src/test/java/za/ac/cput/domain/DoctorTest.java
@@ -0,0 +1,11 @@
+/* DoctorTest.java
+Test for Doctor entity
+Author: Jody Kearns (209023651)
+Date: 26 March 2022 */
+package za.ac.cput.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoctorTest {
+
+}
