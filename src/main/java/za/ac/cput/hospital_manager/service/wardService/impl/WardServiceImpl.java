7291594e2fa7ba1307f2abece5c8aacb80f70297
aismail <ameerismail188@gmail.com>
2022-09-16 16:06:33 -0800

    Task: completed Ward

---
+++ src/main/java/za/ac/cput/hospital_manager/service/wardService/impl/WardServiceImpl.java
@@ -0,0 +1,63 @@
+package za.ac.cput.hospital_manager.service.wardService.impl;
+
+
+/*
+author: Ameer Ismail
+student nr: 218216033
+Service impl: Ward service
+Project 3 Ind Assignment
+*/
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.hospital_manager.domain.Ward;
+import za.ac.cput.hospital_manager.repository.IWardRepository;
+import za.ac.cput.hospital_manager.service.wardService.IWardService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WardServiceImpl implements IWardService
+{
+    private final IWardRepository repo;
+
+    @Autowired
+    public WardServiceImpl(IWardRepository repo)
+    {
+        this.repo = repo;
+    }
+
+    //saving or creating a ward
+    @Override
+    public Ward save(Ward ward)
+    {
+        return this.repo.save(ward);
+    }
+
+    // reading a ward
+    @Override
+    public Optional<Ward> read(Integer id) {
+        return this.repo.findById(id);
+    }
+
+    // deleting
+    @Override
+    public void delete(Ward ward)
+    {
+        this.repo.delete(ward);
+    }
+
+    //get all
+    @Override
+    public List<Ward> findAll()
+    {
+        return this.repo.findAll();
+    }
+
+    @Override
+    public void deleteById(int id)
+    {
+        repo.deleteById(id);
+    }
+}
